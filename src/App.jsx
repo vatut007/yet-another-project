e1c5914885b71ecc8e7a8fa323f1ed3dfb8662d8
vatut007 <vatut007@ya.ru>
2023-10-23 21:41:48 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {Component} from 'react'
+import styles from './app.module.css'
 import DogImage from './dog-image/dog-image'
+import dogImagePath from './images/dog-image.jpeg'
 import Header from './header/header'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className={ styles.app }>
                 <Header/>
-        <DogImage/>
+        <DogImage image = {dogImagePath} />
       </div>
     )
   }
--- src/dog-image/dog-image.jsx
+++ src/dog-image/dog-image.jsx
@@ -1,12 +1,20 @@
 import {Component} from 'react'
-import dogImagePath from '../images/dog-image.jpeg'
+import dogImage from "../dog-image/dog-image.module.css";
 
 class DogImage extends Component {
-    render() {
-      return (
-        <img src={dogImagePath} alt='Грустная собачка Василий' />
-      );
-    }
+  render() {
+    return (
+      <div className={dogImage.card}>
+        <img
+          className={dogImage.image}
+          src={this.props.image}
+          alt="Грустная собачка"
+        />
+        <h2 className={dogImage.title}>{this.props.name}</h2>
+        <span className={dogImage.description}>{this.props.description}</span>
+      </div>
+    );
   }
+}
   
 export default DogImage
